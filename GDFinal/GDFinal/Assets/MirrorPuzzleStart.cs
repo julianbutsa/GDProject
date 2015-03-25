9f84a2f7ef21c603b162f93e93b545b6d6c152d2
julianbutsa <jbruneault@gmail.com>
2015-03-25 09:28:21 -0500

    Consolidated Character Controllers

---
+++ GDFinal/GDFinal/Assets/MirrorPuzzleStart.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MirrorPuzzleStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<ScriptEnabler>().enablePowerNodeController(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ GDFinal/GDFinal/Assets/ScriptEnabler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptEnabler : MonoBehaviour {
+
+	/*
+	 *Disable all scripts at character generation
+	 */
+	void Start () {
+	
+		GetComponent<AppearP> ().enabled = false;
+		GetComponent<PlayerController> ().enabled = false;
+		GetComponent<PlayerControllerPortal> ().enabled = false;
+		GetComponent<WorldInteraction> ().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/*
+	 * Use these functions to enable the scripts you want to use at level startup.
+	 */
+
+	public void enableAppearP(){
+		GetComponent<AppearP> ().enabled = true;
+	}
+
+	public void enableMattsController(){
+		GetComponent<PlayerController> ().enabled = true;
+	}
+
+	public void enablePortalController(){
+		GetComponent<PlayerControllerPortal> ().enabled = true;
+	}
+
+	public void enablePowerNodeController(){
+		GetComponent<WorldInteraction> ().enabled = true;
+		Debug.Log (GetComponent<WorldInteraction> ().enabled);
+	}
+}
