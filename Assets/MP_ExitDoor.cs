7554582baf43d9ee67088ee182244cfdd34c6505
julianbutsa <jbruneault@gmail.com>
2015-04-22 08:14:02 -0500

    Made MIrror Puzzles more pretty

---
+++ Assets/MP_ExitDoor.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MP_ExitDoor : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter(Collider c){
+		Application.LoadLevel ("MainMenuScene");
+	}
+}
