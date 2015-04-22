11917f5f509d6c4aa8fc89d04c3b3d0a94db98de
julianbutsa <jbruneault@gmail.com>
2015-04-22 09:36:37 -0500

    Added laser effects to MirrorPuzzle

--- Assets/MirrorPuzzle/Scripts/PSourceScript.cs
+++ Assets/MirrorPuzzle/Scripts/PSourceScript.cs
@@ -7,9 +7,14 @@ public class PSourceScript : MonoBehaviour {
 	public LayerMask lmask;
 	public bool isActive = false;
 	public GameObject hitObject;
+	public LineRenderer r;
 
 	// Use this for initialization
 	void Start () {
+		r = GetComponent<LineRenderer> ();
+		r.SetWidth (0.2f, 0.2f);
+		r.SetPosition (0, transform.parent.transform.position);
+		r.SetPosition (1, transform.parent.transform.position);
 		hitObject = null;
 		lmask = 1 << 8;
 		//thisray  = new Ray(gameObject.transform.position, GetComponent<Transform>().rotation* transform.localPosition);
@@ -21,27 +26,30 @@ public class PSourceScript : MonoBehaviour {
 		if (isActive) {
 			renderer.material.color = Color.green;		
 		
-		Debug.DrawRay (transform.position, transform.rotation* Vector3.right*100f);
-		//if (isActive) {
-		if (Physics.Raycast (transform.position, transform.rotation* Vector3.right*100f, out hitting, lmask)) {
+			Debug.DrawRay (transform.position, transform.rotation* Vector3.right*100f);
+			//if (isActive) {
+			if (Physics.Raycast (transform.position, transform.rotation* Vector3.right*100f, out hitting, lmask)) {
+				r.SetPosition(1, hitting.collider.gameObject.transform.position);
 
 				if (hitting.collider.tag.Equals ("Node1")) {
 					renderer.material.color = Color.red;
 					hitObject = hitting.collider.gameObject;
 					hitObject.GetComponent<PowerReceiver>().receivingPower = true;
+
 				} else {
 					if(hitObject != null){
 						hitObject.GetComponent<PowerReceiver>().receivingPower = false;
 						hitObject = null;
 					}
 				}
-				} else {
+			} else {
 				if(hitObject != null){
 					hitObject.GetComponent<PowerReceiver>().receivingPower = false;
 					hitObject = null;
 				}
 			}
 		} else {
+			r.SetPosition(1, transform.parent.transform.position);
 			renderer.material.color = Color.grey;		
 		}
 		//}
